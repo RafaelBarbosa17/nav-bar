7270ac838112e017079c8fff88b44e317f59dc33
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2023-01-02 08:19:59 -0300

    Navbar finalizada

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
 const list = document.querySelectorAll('.nav-li');
+const indicator = document.querySelector('.indicator');
+
+const changeIndexI = () => {
+    for (let i = 0; i <= list.length - 1; i++) {
+        if (list[i].classList.contains('active')) {
+            indicator.style.transform = `translateY(-20px) translateX(${i * 86 + 17}px)`
+        }
+    }
+}
+
+changeIndexI()
 
 function activeLink() {
     list.forEach(item => item.classList.remove('active'));
     this.classList.add('active');
-    console.log(this)
 }
 
 list.forEach(item => {
     item.addEventListener('click', activeLink)
+    item.addEventListener('click', changeIndexI)
 })
+
+
